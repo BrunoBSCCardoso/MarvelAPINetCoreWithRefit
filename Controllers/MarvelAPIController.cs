56f4c737f44f28f215a13ac9dc7673a2c11a7d90
BrunoC <bruno.cardoso@kcsitpt.onmicrosoft.com>
2021-11-06 21:47:35 +0000

    Add project files.

---
+++ Controllers/MarvelAPIController.cs
@@ -0,0 +1,24 @@
+﻿using CallMarvelAPIWithRefit.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CallMarvelAPIWithRefit.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MarvelAPIController : ControllerBase
+    {
+        private readonly IMarvelAPIService _marvelAPIService;
+
+        public MarvelAPIController(IMarvelAPIService marvelAPIService)
+        {
+            _marvelAPIService = marvelAPIService ?? throw new ArgumentNullException(nameof(marvelAPIService));
+        }
+
+        public async Task<object> GetCharacteres()
+        {
+            return await _marvelAPIService.GetCharacters();
+        }
+    }
+}
---
+++ Interface/IMarvelAPIService.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using System.Threading.Tasks;
+
+namespace CallMarvelAPIWithRefit.Interface
+{
+    public interface IMarvelAPIService
+    {
+        [Get("/v1/public/characters")]
+        Task<object> GetCharacters();
+    }
+}
